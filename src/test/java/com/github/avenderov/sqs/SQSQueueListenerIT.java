fb02c03ab2d5115dc14fbc742423ca7615c8a528
Alexey Venderov <alexey.venderov@n26.com>
2017-07-16 17:37:54 +0200

    Reorganize tests.

--- src/test/java/com/github/avenderov/sqs/SQSQueueListenerIT.java
+++ src/test/java/com/github/avenderov/sqs/SQSQueueListenerIT.java
@@ -1,24 +1,14 @@
 package com.github.avenderov.sqs;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
-import com.amazonaws.services.sqs.model.CreateQueueResult;
-import com.amazonaws.services.sqs.model.PurgeQueueRequest;
-import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
-import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 import com.amazonaws.services.sqs.model.SendMessageResult;
 import com.github.avenderov.configuration.AmazonSQSProperties;
 import com.github.avenderov.support.AbstractIntegrationTest;
-import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -46,29 +36,6 @@ public class SQSQueueListenerIT extends AbstractIntegrationTest {
 
         await().atMost(1L, TimeUnit.SECONDS)
                 .until(() -> sqsQueueListener.getMessages().get(messageId), equalTo(message));
-    }
-
-    @Test
-    public void shouldPurge() {
-        final String queue = UUID.randomUUID().toString();
-        final CreateQueueResult createQueueResult = amazonSQSAsync.createQueue(queue);
-        final String queueUrl = createQueueResult.getQueueUrl();
-
-        final Set<String> messageIds = IntStream.range(0, 3)
-                .boxed()
-                .map(i -> String.format("message-%d", i))
-                .map(m -> amazonSQSAsync.sendMessage(queueUrl, m))
-                .map(SendMessageResult::getMessageId)
-                .collect(Collectors.toSet());
-
-        assertThat(messageIds).filteredOn(StringUtils::isNotBlank).hasSize(3);
-
-        amazonSQSAsync.purgeQueue(new PurgeQueueRequest().withQueueUrl(queueUrl));
-
-        final ReceiveMessageResult receiveMessageResult = amazonSQSAsync.receiveMessage(
-                new ReceiveMessageRequest().withQueueUrl(queueUrl).withMaxNumberOfMessages(10).withWaitTimeSeconds(1));
-
-        assertThat(receiveMessageResult.getMessages()).isNullOrEmpty();
     }
 
 }
